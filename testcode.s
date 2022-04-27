56473021a563e04895042359dbf90644d09cbcac
Forslund16 <90696858+Forslund16@users.noreply.github.com>
2022-04-27 15:11:28 +0200

    done

---
+++ testcode.s
@@ -0,0 +1,14 @@
+lw $t1, 0($zero)
+lw $t2, 4($zero)
+sll $zero, $zero, 0
+sll $zero, $zero, 0
+sll $zero, $zero, 0
+add $t3, $t1, $t2
+sll $zero, $zero, 0
+sll $zero, $zero, 0
+sll $zero, $zero, 0
+add $t3, $t1, $t3
+sll $zero, $zero, 0
+sll $zero, $zero, 0
+sll $zero, $zero, 0
+sw $t3, 12($zero)
